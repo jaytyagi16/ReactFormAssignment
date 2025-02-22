402f0a3ea65d938ab08af9b57d77a0677c2fc92f
Jay Tyagi <jaytyagi16@gmail.com>
2025-02-22 09:58:34 +0530

    Optimised Logic For Login & Signup

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,93 +1,94 @@
-import React from 'react'
-import { useForm } from 'react-hook-form'
-import * as Yup from "yup"
-import { yupResolver } from '@hookform/resolvers/yup'
-import { useNavigate } from 'react-router'
-
-type loginForm = {
-  email: string;
-  password: string
-}
+import React from "react";
+import { useForm } from "react-hook-form";
+import * as Yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useNavigate } from "react-router";
+import { loginForm } from "../types/Forms";
+import { UserStore } from "./SignupForm";
 
 const schemaValidation = Yup.object({
-  email: Yup.string().email("Not in email format").required("Email is required"),
-  password: Yup.string().required("Password is required")
-})
+  email: Yup.string()
+    .email("Not in email format")
+    .required("Email is required"),
+  password: Yup.string().required("Password is required"),
+});
 
 const LoginForm: React.FC = () => {
   const {
     register,
     handleSubmit,
-    formState: {errors}
+    formState: { errors },
   } = useForm<loginForm>({
-    resolver: yupResolver(schemaValidation)
-  })
+    resolver: yupResolver(schemaValidation),
+  });
 
   const navigate = useNavigate();
 
   const onSubmit = (data: loginForm) => {
-    console.log("form data", data);
-
     const userEmail = data.email;
     const userPassword = data.password;
-    let credentials;
-    const storedCredentials = localStorage.getItem("credentials");
-    
-    if (storedCredentials) {
-      credentials = JSON.parse(storedCredentials);
+
+    const storedUsers = localStorage.getItem("allUsers");
+    if (!storedUsers) {
+      alert("No users found. Please sign up first.");
+      return;
     }
-    
-    if(userEmail === credentials.email && userPassword === credentials.password){
-      alert("Logged in successful");
-      navigate('/home');
+
+    const users: UserStore = JSON.parse(storedUsers);
+
+    const foundUser = users.user.find(
+      (user) => user.email === userEmail && user.password === userPassword
+    );
+
+    if (foundUser) {
+      alert("Login successful");
+      navigate("/home");
     } else {
       alert("Incorrect email or password");
     }
-  }
+  };
 
   return (
     <div>
-
-      <h2 className='font-bold text-2xl'>Sign in to Dribble</h2>
+      <h2 className="font-bold text-2xl">Sign in to Dribble</h2>
 
       <form
         onSubmit={handleSubmit(onSubmit)}
-        className='mt-10 flex flex-col gap-8 lg:w-[446px]'
+        className="mt-10 flex flex-col gap-8 lg:w-[446px]"
       >
         {/* email */}
-        <div className='flex flex-col gap-1'>
-          <label className='font-bold'>Email</label>
+        <div className="flex flex-col gap-1">
+          <label className="font-bold">Email</label>
           <input
             {...register("email")}
-            className='border-2 border-gray-300 rounded px-2 h-9'
+            className="border-2 border-gray-300 rounded px-2 h-9"
           />
-          {
-            errors.email && <p className='text-red-500 text-sm'>{errors.email.message}</p>
-          }
+          {errors.email && (
+            <p className="text-red-500 text-sm">{errors.email.message}</p>
+          )}
         </div>
         {/* password */}
-        <div className='flex flex-col gap-1'>
-          <label className='font-bold'>Password</label>
+        <div className="flex flex-col gap-1">
+          <label className="font-bold">Password</label>
           <input
-            type='password'
+            type="password"
             {...register("password")}
-            className='border-2 border-gray-300 rounded px-2 h-9'
+            className="border-2 border-gray-300 rounded px-2 h-9"
           />
-          {
-            errors.password && <p className='text-red-500 text-sm'>{errors.password.message}</p>
-          }
+          {errors.password && (
+            <p className="text-red-500 text-sm">{errors.password.message}</p>
+          )}
         </div>
-        
-        <button 
-          type="submit" 
+
+        <button
+          type="submit"
           className="bg-slate-900 text-white py-4 px-4 hover:bg-slate-700 font-bold rounded-full cursor-pointer"
         >
           Log in
         </button>
       </form>
-
     </div>
-  )
-}
+  );
+};
 
-export default LoginForm
+export default LoginForm;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
+import React from "react";
+import Navbar from "../components/Navbar";
 
 const Home: React.FC = () => {
   return (
-    <div className='max-w-screen'>
+    <div className="max-w-screen">
+      <div className="w-[90%] mx-auto mt-2">
+        <Navbar />
 
-      <div className='w-[90%] mx-auto mt-2'>
-        <Navbar/>
+        <h1 className="text-center text-3xl mt-40">Welcome To Dribble</h1>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
